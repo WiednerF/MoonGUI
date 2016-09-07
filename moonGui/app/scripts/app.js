22a287bb95bab8d5986e0816140704d41f63c90c
WiednerF <florian.wiedner@tum.de>
2016-09-07 11:46:41 +0200

    Added first frontend structure

---
+++ moonGui/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name moonGuiApp
+ * @description
+ * # moonGuiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('moonGuiApp', [
+    'ngAnimate'
+  ]);
---
+++ moonGui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moonGuiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moonGuiApp
+ */
+angular.module('moonGuiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
