343e4d82a4da42b8938496c0513811e387a4a4a0
WiednerF <florian.wiedner@tum.de>
2016-09-23 11:41:40 +0200

    Add Alert type

--- moonGui2/src/app/main-alert/main-alert.component.ts
+++ moonGui2/src/app/main-alert/main-alert.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit,Input } from '@angular/core';
+import {NgbSelfClosingAlertConfig} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-main-alert',
   templateUrl: './main-alert.component.html',
-  styleUrls: ['./main-alert.component.css']
+  styleUrls: ['./main-alert.component.css'],
+    providers: [NgbSelfClosingAlertConfig]
 })
 export class MainAlertComponent implements OnInit {
 
   @Input()
-  public alerts: Array<{type:string,content:string}> = [{type:'warning',content:'This alert will close automatically after 5 seconds'}];
+  public alerts: Array<{type:string,content:string}> = [{type:"info",content:"test"}];
 
-  constructor() { }
-
-  ngOnInit() {
+    /**
+     * Configuration of the Alert Time
+     * @param selfClosingAlertConfig
+     */
+  constructor(selfClosingAlertConfig:NgbSelfClosingAlertConfig) {
+      selfClosingAlertConfig.dismissible = true;
+      selfClosingAlertConfig.dismissOnTimeout = 5000;
   }
 
+  ngOnInit() {
+
+  }
+
+  /**
+   * Add a new Alert to the see type
+   * @param type Danger, info, warning, success
+   * @param content The Content to be displayed
+   */
   public addAlert(type:string,content:string) {
     this.alerts.push({type:type,content:content});
   }
 }
+
