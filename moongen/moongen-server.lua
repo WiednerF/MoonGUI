d26f69c9b16d99b469e0c0c9912a7277abfa645a
Florian Wiedner <florian.wiedner@tum.de>
2016-09-27 08:38:18 -0700

    Changed to new software timestamping model

--- moongen/moongen-server.lua
+++ moongen/moongen-server.lua
@@ -9,36 +9,82 @@ local log = require "log"
 local device = require "device"
 local zmq = require "zmq"
 
+
+
+local PKT_SIZE = 60
+
+local NUM_PKTS = 10^6
+--START PIPE
+local ctx = zmq.init()
+local s = ctx:socket(zmq.REQ)
+  s:connect("tcp://localhost:5555")
+--END PIPE
+
 function configure(parser)
 	parser:argument("rxDev","The device to receive from"):convert(tonumber)
 	parser:argument("txDev","The Device to send to"):convert(tonumber)
-	parser:argument("port","The port for the Webserver"):convert(tonumber)
 	parser:argument("execution","The number of the current execution"):convert(tonumber)
 end
 
 
 function master(args)
-	--TODO Test
-	--PIPE Part
-	local ctx = zmq.init()
-	local s = ctx:socket(zmq.REQ)
-	s:connect("tcp://localhost:5555")
-
+	--TODO TEST
+	--TODO Packet number, size, rateLimit as Config parameters
 	local txDev = device.config{port = args.txDev,dropEnable = false}
 	local rxDev = device.config{port = args.rxDev, dropEnable = false}
 	device.waitForLinks()
-	mg.startTask("dumpSlave",rxDev:getRxQueue(0),txDev:getTxQueue(0))
+	mg.startTask("txTimestamper", txDev:getTxQueue(0))
+	mg.startTask("rxTimestamper", rxDev:getRxQueue(0))
 	mg.waitForTasks()
 	s:close()
 	ctx:term()
 end
 
-function dumpSlave(rxQueue,txQueue)
-	local timestamper = ts:newTimestamper(txQueue, rxQueue)
-	local hist = hist:new()
-	while mg.running() do
-		hist:update(timestamper:measureLatency())
-		print(hist:avg())
+--TODO Load
+function txTimestamper(queue)
+	local mem = memory.createMemPool(function(buf)
+		-- just to use the default filter here
+		-- you can use whatever packet type you want
+		buf:getUdpPtpPacket():fill{
+		}
+	end)
+	mg.sleepMillis(1000) -- ensure that the load task is running
+	local bufs = mem:bufArray(1)
+	local rateLimit = timer:new(0.0000000001) -- 1000kpps timestamped packets
+	local i = 0
+	while i < NUM_PKTS and mg.running() do
+		bufs:alloc(PKT_SIZE)
+		queue:sendWithTimestamp(bufs)
+		rateLimit:wait()
+		rateLimit:reset()
+		i = i + 1
 	end
-	hist:print()
+	mg.sleepMillis(500)
+	mg.stop()
+end
+
+function rxTimestamper(queue)
+	local tscFreq = mg.getCyclesFrequency()
+	local bufs = memory.bufArray(64)
+	-- use whatever filter appropriate for your packet type
+	queue:filterUdpTimestamps()
+	local results = {}
+	local rxts = {}
+	while mg.running() do
+		local numPkts = queue:recvWithTimestamps(bufs)
+		for i = 1, numPkts do
+			local rxTs = bufs[i].udata64
+			local txTs = bufs[i]:getSoftwareTxTimestamp()
+			results[#results + 1] = tonumber(rxTs - txTs) / tscFreq * 10^9 -- to nanoseconds
+			rxts[#rxts + 1] = tonumber(rxTs)
+			print(results[#results].." rxTS: "..rxts[#rxts])
+			s:send(tostring(results[#results]))--TODO All Values
+		end
+		bufs:free(numPkts)
+	end
+	local f = io.open("pings.txt", "w+")
+	for i, v in ipairs(results) do
+		f:write(v .. "\n")
+	end
+	f:close()
 end
--- moongen/moongen.sh
+++ moongen/moongen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo ../setup-hugetlbfs.sh
-sudo ../MoonGen moongen/moongen-server.lua 0 1 103546 50
+sudo ../MoonGen moongen/moongen-server.lua 0 1 50
