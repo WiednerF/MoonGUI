d7fd9a9f771471538a7786fabaa918c2f6ad8337
WiednerF <florian.wiedner@tum.de>
2016-09-29 00:10:24 +0200

    Changed showing Log behaviour

--- moonGui2/src/app/log-viewer/log-viewer.component.ts
+++ moonGui2/src/app/log-viewer/log-viewer.component.ts
@@ -1,74 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MoonGenService} from "../services/moon-gen.service";
 import {Observable} from "rxjs";
 import {MoonConnectServiceService} from "../services/moon-connect-service.service";
+declare var ansi_up: any;
 
 @Component({
-  selector: 'app-log-viewer',
-  templateUrl: './log-viewer.component.html',
-  styleUrls: ['./log-viewer.component.css']
+    selector: 'app-log-viewer',
+    templateUrl: './log-viewer.component.html',
+    styleUrls: ['./log-viewer.component.css']
 })
 /**
  * This class Generates the Viewer for the Real time log file
  */
 export class LogViewerComponent implements OnInit {
 
-    private executionNumber:number=-1;
-    private lineNumber:number=0;
-    private response:boolean=true;
+    private executionNumber: number = -1;
+    private lineNumber: number = 0;
+    private response: boolean = true;
     private log: any = [];
 
-  constructor(private moonGenService:MoonGenService,private connectService:MoonConnectServiceService) {
+    constructor(private moonGenService: MoonGenService, private connectService: MoonConnectServiceService) {
 
-  }
+    }
 
-  ngOnInit() {
-      this.runningLogFile();
-  }
+    ngOnInit() {
+        this.runningLogFile();
+    }
 
     /**
      * Starts the Process for Fetching Log File
      */
-  private runningLogFile(){
-      Observable.interval(100).subscribe(()=>{
-          if(this.moonGenService.getRunning()==true){
-            if(this.executionNumber!=this.moonGenService.getExecutionNumber()){
-                this.executionNumber=this.moonGenService.getExecutionNumber();
-                if(this.executionNumber!=null)this.cleanLog();
-                this.initiateLog();
+    private runningLogFile() {
+        Observable.interval(100).subscribe(()=> {
+            if (this.moonGenService.getRunning() == true) {
+                if (this.executionNumber != this.moonGenService.getExecutionNumber()) {
+                    this.executionNumber = this.moonGenService.getExecutionNumber();
+                    if (this.executionNumber != null)this.cleanLog();
+                    this.initiateLog();
+                }
+                if (this.response) {
+                    this.getLog();
+                }
             }
-            if(this.response){
-                this.getLog();
-            }
-          }
-      });
-  }
+        });
+    }
 
     /**
      * Get the Log from extern
      */
-  private getLog(){
-      let logFile=this.moonGenService.getLogFile(this.lineNumber);
-        this.response=false;//TODO Problems Getting Array correct
-        if(logFile!=null){
-            logFile.map(response=>response.json()).subscribe(response=>{this.lineNumber=response.lines;this.response=true;this.log.push(response.log);console.log(this.log);console.log(response)},(error)=>{this.connectService.addAlert("danger","Log File Error: "+error);this.response=false});
+    private getLog() {
+        let logFile = this.moonGenService.getLogFile(this.lineNumber);
+        this.response = false;//TODO Problems Getting Array correct
+        if (logFile != null) {
+            logFile.map(response=>response.json()).subscribe(response=> {
+                this.lineNumber = response.lines;
+                this.response = true;
+                var result = response.log;
+                for(var i=0;i<result.length();i++){
+                    result[i]=ansi_up.ansi_to_html(result[i]);
+                }
+                this.log.push(response.log);
+            }, (error)=> {
+                this.connectService.addAlert("danger", "Log File Error: " + error);
+                this.response = false
+            });
         }
         //tODO
-  }
+    }
+
     /**
      * Clean the old log for a new one
      */
-  private cleanLog(){
-      //TODO
-  }
+    private cleanLog() {
+        //TODO
+    }
 
     /**
      * Initiate the DOM for the Log
      */
-  private initiateLog(){
-      this.lineNumber=0;
-        this.log=[];
-      //TODO
-  }
+    private initiateLog() {
+        this.lineNumber = 0;
+        this.log = [];
+        //TODO
+    }
 
 }
