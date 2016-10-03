4963c875126a83b567aae2ffee942dc9a7dbd1d2
WiednerF <florian.wiedner@tum.de>
2016-10-03 17:56:48 +0200

    Added Timer for performance Reason

--- moonGui2/src/app/config/system/system.component.ts
+++ moonGui2/src/app/config/system/system.component.ts
@@ -12,7 +12,7 @@ export class SystemComponent {
   private sys:{arch:string,os:string,user:string,cores:number,lua:{version:string,status:boolean}}={lua:{}};
 
   constructor(public moonConnect:MoonConnectServiceService) {
-      this.getInformation(moonConnect);
+      Observable.timer(2000).take(1).subscribe(()=>this.getInformation(moonConnect));
   }
   getInformation(moonConnect:MoonConnectServiceService){
       moonConnect.get("/rest/system/").map(result => result.json()).subscribe(result=>this.sys=result,()=>{moonConnect.addAlert("info","Error Fetching System Information");Observable.timer(20000).take(1).subscribe(()=>this.getInformation(moonConnect));});
