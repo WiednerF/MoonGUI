162f279453fe9f5b0744df9f4a6e60d93ca6a3ca
WiednerF <florian.wiedner@tum.de>
2016-10-03 17:54:36 +0200

    Added Function for System Information collection

---
+++ moonGui2/src/app/config/system/system.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MoonConnectServiceService} from "../../services/moon-connect-service.service";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-system',
+  templateUrl: './system.component.html',
+  styleUrls: ['./system.component.css']
+})
+export class SystemComponent {
+
+  private sys:{arch:string,os:string,user:string,cores:number,lua:{version:string,status:boolean}}={lua:{}};
+
+  constructor(public moonConnect:MoonConnectServiceService) {
+      this.getInformation(moonConnect);
+  }
+  getInformation(moonConnect:MoonConnectServiceService){
+      moonConnect.get("/rest/system/").map(result => result.json()).subscribe(result=>this.sys=result,()=>{moonConnect.addAlert("info","Error Fetching System Information");Observable.timer(20000).take(1).subscribe(()=>this.getInformation(moonConnect));});
+
+  }
+
+}
