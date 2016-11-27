8db1e302d5e47a19e48f4e4c6e19eff080dbfd47
Florian Wiedner <florian.wiedner@tum.de>
2016-11-27 04:15:45 -0800

    Changed to same LuaJIT version that MoonGen uses

--- start-webserver.sh
+++ start-webserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo ../setup-hugetlbfs.sh
 sudo ../bind-interfaces.sh
-sudo luajit webserver.lua ${1:-8080}  
+sudo ../libmoon/deps/luajit/usr/local/bin/luajit-2.1.0-beta2 webserver.lua ${1:-8080}  
 #luajit webserver.lua
