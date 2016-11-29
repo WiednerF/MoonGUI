bdd48b46140c07962effbaaca3a5b247d06a0913
WiednerF <florian.wiedner@tum.de>
2016-11-29 12:00:20 +0100

    Refactored and documentation of moonHistoryService

--- moongui/src/app/services/moon-history.service.ts
+++ moongui/src/app/services/moon-history.service.ts
@@ -1,17 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {MoonConnectService} from "./moon-connect.service";
+import {Observable} from "rxjs";
+import {Response} from "@angular/http";
 
+/**
+ * This Service connects the frontend representation with the history API
+ */
 @Injectable()
 export class MoonHistoryService {
 
-  constructor(public moonConnect:MoonConnectService) { }
-
-
-  public clearAll():void{
-    let del = this.moonConnect.del("/rest/history/");
-    if(del!=null){
-       del.subscribe(()=>{this.moonConnect.addAlert("success","Successfully deleted all history files")},(error)=>{this.moonConnect.addAlert("danger","Error in Deleting history files"+error)});
+    /**
+     *
+     * @param moonConnect The Connection Service to the Server
+     */
+    constructor(public moonConnect: MoonConnectService) {
     }
-  }
+
+
+    /**
+     * This methods clears all data previously stored in the history folder on the server
+     */
+    public clearAll(): void {
+        let del:Observable<Response> = this.moonConnect.del("/rest/history/");//The request is started
+        if (del != null) {//del is only null, if the connection is not possible
+            del.subscribe(() => {
+                this.moonConnect.addAlert("success", "Successfully deleted all history files")
+            }, (error) => {
+                this.moonConnect.addAlert("error", "Error in Deleting history files: " + error)
+            });
+        }
+    }
 
 }
