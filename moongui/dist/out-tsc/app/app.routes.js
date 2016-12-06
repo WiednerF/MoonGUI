2321e55944ef9071e718369bb80d2d5cafffe62f
Florian Wiedner <florian.wiedner@tum.de>
2016-12-06 04:58:26 -0800

    Removed binaries from git

--- moongui/dist/out-tsc/app/app.routes.js
+++
@@ -1,7 +0,0 @@
-import { RouterModule } from '@angular/router';
-import { MainComponent } from "./main/main.component";
-export var appRoutes = [
-    { path: '**', component: MainComponent }
-];
-export var routing = RouterModule.forRoot(appRoutes);
-//# sourceMappingURL=D:/Florian/Documents/Bachelor Informatik/7.Semester/Bachelorarbeit/github/ba-wiedner/webserver/moongui/src/app/app.routes.js.map
--- moongui/dist/out-tsc/app/index.js
+++
@@ -1,3 +0,0 @@
-export * from './app.component';
-export * from './app.module';
-//# sourceMappingURL=D:/Florian/Documents/Bachelor Informatik/7.Semester/Bachelorarbeit/github/ba-wiedner/webserver/moongui/src/app/index.js.map
