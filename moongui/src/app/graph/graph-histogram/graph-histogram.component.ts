6e0826bec8d1ee09bfab4a3fa3f784d966108c4d
WiednerF <florian.wiedner@tum.de>
2016-12-14 17:17:37 +0100

    Documented and refactored the classes for showing graphs

--- moongui/src/app/graph/graph-histogram/graph-histogram.component.ts
+++ moongui/src/app/graph/graph-histogram/graph-histogram.component.ts
@@ -5,137 +5,226 @@ declare let Plotly: any;//Declares the Variables already defined to be usable in
 declare let document: any;
 
 @Component({
-  selector: 'app-graph-histogram',
-  templateUrl: './graph-histogram.component.html'
+    selector: 'app-graph-histogram',
+    templateUrl: './graph-histogram.component.html'
 })
 /**
- * Generates a standard graph template for a histogram
+ * Generates a standard graph template for a histogram based on the Plotly.JS library
  */
-export class GraphHistogramComponent implements OnChanges,AfterViewInit {
+export class GraphHistogramComponent implements OnChanges, AfterViewInit {
+    /**
+     * The Input for the data points
+     */
+    @Input() public points: any;
+    /**
+     * The input for the title of the diagram
+     */
+    @Input() public title: string;
+    /**
+     * The size of the displayed elements
+     */
+    @Input() public size: number;
+    /**
+     * The Div id, to be used in this view
+     */
+    @Input() public id: string;
+    /**
+     * All Input parameters to define the range with default values for minimum, maximum and Step
+     * @type {number}
+     */
+    @Input() public rangeMin: number = 0;
+    @Input() public rangeMax: number = 1000000000000;
+    @Input() public rangeStep: number = 0.0000001;
 
-    //TODO From here
-  @Input() public points:any;
-  @Input() public title:string;
-    @Input() public size:number;
-    @Input() public id:string;
-    @Input() public rangeMin:number=0;
-    @Input() public rangeMax:number=1000000000000;
-    @Input() public rangeStep:number=0.0000001;
-
-    @ViewChild('editModal') public editModal:ModalDirective;
-    private xAxisRange:string = "Automatic";
+    /**
+     * The View of the Modal for editing
+     */
+    @ViewChild('editModal') public editModal: ModalDirective;
+    /**
+     * Writing of the button for change between Manual and automatic
+     * @type {string}
+     */
+    private xAxisRange: string = "Automatic";
+    /**
+     * The Percentile change value
+     * @type {boolean}
+     */
     private xAxisPercentile: boolean = false;
+    /**
+     * The Percentile and number change Button value
+     * @type {string}
+     */
     private xAxisPercentileTitle: string = "Number";
+    /**
+     * The Value of the percentile
+     * @type {number}
+     */
     private xAxisPercentileValue: number = 99;
+    /**
+     * The Layout object of the graph, more information can be found in the internet on the Plotly website
+     * @type {any}}
+     */
+    private layout: any = {
+        title: this.title,
+        bargap: 0.05,
+        bargroupgap: 0.2,
+        yaxis: {title: "Count"},
+        xaxis: {title: "Value", autorange: true, range: [10, 50], autosize: true}
+    };
+    /**
+     * The Data object of the graph and all its traces, more information can be found in the internet on the Plotly website
+     * @type {Array}
+     */
+    private data: any = [];
 
-
-
-  private configuration:any={showLink: false, displaylogo: false};
-  private layout:any= {title: this.title,bargap: 0.05,bargroupgap:0.2,yaxis:{title: "Count"},xaxis:{title:"Value",autorange:true, range: [10,50] ,autosize: true}};
-  private data:any=[];
-
-  constructor(public element:ElementRef) {
-  }
-
-  ngAfterViewInit() {
-      this.element.nativeElement.children[0].children[0].setAttribute("id", this.id);
-      for(let i:number=0;i<this.points.length;i++) {
-          this.data.push({
-              name: this.points[i].title,
-              x: this.points[i].x,
-              type: 'histogram',
-              autobinx: false,
-              xbins: {start: Math.min.apply(Math, this.points[i].x), end: Math.max.apply(Math, this.points[i].x), size: this.size}
-          });
-
-      }
-      console.log(this.data);
-      this.layout.title=this.title;
-      Plotly.newPlot(this.id, this.data,this.layout,this.configuration);
-  }
-  ngOnChanges(changes){
-      if(this.data!=[]&& this.data.length!=0) {
-          var graphDiv=document.getElementById(this.id);
-          if (changes.points) {
-              var update1:any = {x:[],xbins:[]};
-              for(let i:number=0;i<changes.points.currentValue.length;i++) {
-                  update1.x.push(changes.points.currentValue[i].x);
-                  update1.xbins.push({
-                      start: Math.min.apply(Math, changes.points.currentValue[i].x),
-                      end: Math.max.apply(Math, changes.points.currentValue[i].x),
-                      size: this.size
-                  });
-              }
-              Plotly.restyle(graphDiv,update1);
-              if(this.xAxisPercentile&&!this.layout.xaxis.autorange){
-                  this.changeRangePercentile();
-              }
-          }
-
-          if (changes.title) {
-              var update = {
-                  title: changes.title.currentValue
-              };
-              Plotly.relayout(graphDiv,update);
-          }
-      }
-  }
-  private changeYAxisTitle($event){
-      var graphDiv=document.getElementById(this.id);
-      Plotly.relayout(graphDiv, {yaxis:{title:$event}});
-  }
-
-    private changeXAxisTitle($event){
-        var graphDiv=document.getElementById(this.id);
-        this.layout.xaxis.title = $event;
-        Plotly.relayout(graphDiv, {xaxis:this.layout.xaxis});
+    /**
+     *
+     * @param element The element reference of this element
+     */
+    constructor(public element: ElementRef) {
     }
-    private changeXAxisAutoRange($event){
-        var graphDiv=document.getElementById(this.id);
+
+    /**
+     * Returns the Element of the HTML page containing the graph
+     * @returns {HTMLElement}
+     */
+    private getElement():HTMLElement{
+        return document.getElementById(this.id);
+    }
+
+    ngAfterViewInit() {
+        this.element.nativeElement.children[0].children[0].setAttribute("id", this.id);//Sets the id for connecting to the div
+        for (let i: number = 0; i < this.points.length; i++) {
+            this.data.push({//Pushes the data for each trace
+                name: this.points[i].title,
+                x: this.points[i].x,
+                type: 'histogram',
+                autobinx: false,
+                xbins: {
+                    start: Math.min.apply(Math, this.points[i].x),
+                    end: Math.max.apply(Math, this.points[i].x),
+                    size: this.size
+                }
+            });
+
+        }
+        this.layout.title = this.title;//Setting the layout title
+        Plotly.newPlot(this.id, this.data, this.layout, {showLink: false, displaylogo: false});//Plotting the graph initially
+    }
+
+    ngOnChanges(changes) {
+        if (this.data != [] && this.data.length != 0) {//Only change if data is already defined
+            let graphDiv:HTMLElement = this.getElement();
+            if (changes.points) {//Changes of the points
+                let update1: any = {x: [], xbins: []};
+                for (let i: number = 0; i < changes.points.currentValue.length; i++) {
+                    update1.x.push(changes.points.currentValue[i].x);//Calculates the changes for each trace again
+                    update1.xbins.push({
+                        start: Math.min.apply(Math, changes.points.currentValue[i].x),
+                        end: Math.max.apply(Math, changes.points.currentValue[i].x),
+                        size: this.size
+                    });
+                }
+                Plotly.restyle(graphDiv, update1);//Update and change range of percentile, if wanted
+                if (this.xAxisPercentile && !this.layout.xaxis.autorange) {
+                    this.changeRangePercentile();
+                }
+            }
+
+            if (changes.title) {//Changes of the title
+                this.layout.title = changes.title.currentValue;
+                Plotly.relayout(graphDiv, this.layout);
+            }
+        }
+    }
+
+    /**
+     * Redraw the layout after changing the YAxis title
+     * @param $event
+     */
+    private changeYAxisTitle($event) {
+        Plotly.relayout(this.getElement(), {yaxis: {title: $event}});
+    }
+
+    /**
+     * Redraw the layout after changing the XAxis title
+     * @param $event
+     */
+    private changeXAxisTitle($event) {
+        this.layout.xaxis.title = $event;
+        Plotly.relayout(this.getElement(), {xaxis: this.layout.xaxis});
+    }
+
+    /**
+     * Change the layout from Automatic to manual or in the other direction
+     * @param $event
+     */
+    private changeXAxisAutoRange($event) {
         this.xAxisRange = $event ? "Automatic" : "Manual";
         this.layout.xaxis.autorange = $event;
-        Plotly.relayout(graphDiv, {xaxis:this.layout.xaxis});
+        Plotly.relayout(this.getElement(), {xaxis: this.layout.xaxis});
     }
-    private changeXAxisPercentile($event){
+
+    /**
+     * Change the layout from Number to percentile or in the other direction
+     * @param $event
+     */
+    private changeXAxisPercentile($event) {
         this.xAxisPercentileTitle = $event ? "Percentile" : "Number";
         this.xAxisPercentile = $event;
         this.changeRangePercentile();
     }
-    private changeXAxisPercentileRange($event){
+
+    /**
+     * Change the range of the percentile value
+     * @param $event
+     */
+    private changeXAxisPercentileRange($event) {
         this.xAxisPercentileValue = $event;
         this.changeRangePercentile();
     }
 
-    private changeXAxisRange($event){
-        var graphDiv=document.getElementById(this.id);
+    /**
+     * Change the Number of the XAxis range for both values
+     * @param $event
+     */
+    private changeXAxisRange($event) {
         this.layout.xaxis.range = $event;
-        let newRange = [this.layout.xaxis.range[0],this.layout.xaxis.range[1]];
-        this.layout.xaxis.range = newRange;
-        Plotly.relayout(graphDiv, {xaxis:this.layout.xaxis});
-    }
-    private changeXAxisRangeNumber($event,id:number){
-        var graphDiv=document.getElementById(this.id);
-        this.layout.xaxis.range[id] = $event;
-        let newRange = [this.layout.xaxis.range[0],this.layout.xaxis.range[1]];
-        this.layout.xaxis.range = newRange;
-        Plotly.relayout(graphDiv, {xaxis:this.layout.xaxis});
+        this.layout.xaxis.range = [this.layout.xaxis.range[0], this.layout.xaxis.range[1]];
+        Plotly.relayout(this.getElement(), {xaxis: this.layout.xaxis});
     }
 
-    private changeTitle($event){
-        var graphDiv=document.getElementById(this.id);
-        Plotly.relayout(graphDiv, {title:$event});
+    /**
+     * Change the Number of the XAxis range for one value, defined by the id
+     * @param $event
+     * @param id
+     */
+    private changeXAxisRangeNumber($event, id: number) {
+        this.layout.xaxis.range[id] = $event;
+        this.layout.xaxis.range = [this.layout.xaxis.range[0], this.layout.xaxis.range[1]];
+        Plotly.relayout(this.getElement(), {xaxis: this.layout.xaxis});
     }
-    private getPropRange(id:number){
+
+    /**
+     * returns the Property of the Range part defined by the ID
+     * @param id the ID
+     * @returns {any}
+     */
+    private getPropRange(id: number) {
         return this.layout.xaxis.range[id];
     }
 
-    private changeRangePercentile(){
-        if(this.data[0]){
-            var graphDiv=document.getElementById(this.id);
-            let result:[number]=this.data[0].x;
-            result.sort(function(a,b){return a-b });
-            this.layout.xaxis.range = [result[0],result[Math.round(result.length*(this.xAxisPercentileValue/100.0))]];
-            Plotly.relayout(graphDiv, {xaxis:this.layout.xaxis});
+    /**
+     * Recalculates the Percentile Range and Re Layout after change of the number or the data values
+     */
+    private changeRangePercentile() {
+        if (this.data[0]) {
+            let result: [number] = this.data[0].x;
+            result.sort(function (a, b) {
+                return a - b
+            });
+            this.layout.xaxis.range = [result[0], result[Math.round(result.length * (this.xAxisPercentileValue / 100.0))]];
+            Plotly.relayout(this.getElement(), {xaxis: this.layout.xaxis});
         }
     }
 
--- moongui/src/app/graph/graph-line/graph-line.component.ts
+++ moongui/src/app/graph/graph-line/graph-line.component.ts
@@ -1,84 +1,123 @@
 import {Component, Input, OnChanges, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
 import {ModalDirective} from "ng2-bootstrap";
 
-declare var Plotly: any;
-declare var document: any;
+declare let Plotly: any;
+declare let document: any;
 
 @Component({
-  selector: 'app-graph-line',
-  templateUrl: './graph-line.component.html'
+    selector: 'app-graph-line',
+    templateUrl: './graph-line.component.html'
 })
 /**
  * Generates a standard graph template for a line graph
  */
 export class GraphLineComponent implements OnChanges,AfterViewInit {
-  @Input() public points:any;
-  @Input() public title:string;
-  @Input() public id:string;
-  @Input() public max:number;
-    @ViewChild('editModal') public editModal:ModalDirective;
+    /**
+     * The Input for the data points
+     */
+    @Input() public points: any;
+    /**
+     * The input for the title of the diagram
+     */
+    @Input() public title: string;
+    /**
+     * The Div id, to be used in this view
+     */
+    @Input() public id: string;
+    /**
+     * The maximum of entries visible in the graph
+     */
+    @Input() public max: number;
+    /**
+     * The Modal for closing and opening for editing of the graph
+     */
+    @ViewChild('editModal') public editModal: ModalDirective;
+    /**
+     * The Layout object of the graph, more information can be found in the internet on the Plotly website
+     * @type {any}}
+     */
+    private layout: any = {
+        title: this.title,
+        bargap: 0.05,
+        bargrourgap: 0.2,
+        yaxis: {title: "Count"},
+        xaxis: {title: "Value", autorange: false, range: [0, 1000000]}
+    };
+    /**
+     * The Data object of the graph and all its traces, more information can be found in the internet on the Plotly website
+     * @type {Array}
+     */
+    private data: any = [];
 
-  private configuration:any={showLink: false,displaylogo: false};
-  private layout:any= {title: this.title,bargap: 0.05,bargrourgap:0.2,yaxis:{title: "Count"},xaxis:{title:"Value", autorange: false,range:[0,1000000]}};
-  private data:any=[];
-
-    constructor(public element:ElementRef) {
+    /**
+     *
+     * @param element The element reference of this element
+     */
+    constructor(public element: ElementRef) {
     }
+
+    /**
+     * Returns the Element of the HTML page containing the graph
+     * @returns {HTMLElement}
+     */
+    private getElement(): HTMLElement {
+        return document.getElementById(this.id);
+    }
+
 
     ngAfterViewInit() {
-        this.element.nativeElement.children[0].setAttribute("id", this.id);
-        for(let i:number=0;i<this.points.length;i++){
-            this.data.push({x: this.points[i].x, y: this.points[i].y, name:this.points[i].title, mode: 'lines',});
+        this.element.nativeElement.children[0].setAttribute("id", this.id);//Changes the ID of the DIV for the access for re-layouting the graph
+        for (let i: number = 0; i < this.points.length; i++) {//Pushes for each trace
+            this.data.push({x: this.points[i].x, y: this.points[i].y, name: this.points[i].title, mode: 'lines',});
         }
-        this.layout.title=this.title;
-        Plotly.newPlot(this.id, this.data,this.layout,this.configuration);
-
-
+        this.layout.title = this.title;//The name of the diagram
+        Plotly.newPlot(this.id, this.data, this.layout, {showLink: false, displaylogo: false});//Plot the diagram the first time
     }
-    ngOnChanges(changes){
-        if(this.data!=[]&& this.data.length!=0) {
-            var graphDiv=document.getElementById(this.id);
-            if (changes.points) {
-                var update1:any = {x:[],y:[]};
-                for(let i:number=0;i<changes.points.currentValue.length;i++) {
+
+    ngOnChanges(changes) {
+        if (this.data != [] && this.data.length != 0) {
+            let graphDiv: HTMLElement = this.getElement();
+            if (changes.points) {//Changes in the points
+                let update1: any = {x: [], y: []};
+                for (let i: number = 0; i < changes.points.currentValue.length; i++) {
                     update1.x.push(changes.points.currentValue[i].x);
                     update1.y.push(changes.points.currentValue[i].y);
                 }
-                Plotly.restyle(graphDiv,update1);
-         if(changes.points.currentValue.length>0) {
-             if (changes.points.currentValue[0].x.length > this.max) {
-                 this.layout.xaxis.range =[changes.points.currentValue[0].x[changes.points.currentValue[0].x.length - (this.max)], changes.points.currentValue[0].x[changes.points.currentValue[0].x.length - 1]];
-                 Plotly.relayout(graphDiv, this.layout)
-             } else {
-                 this.layout.xaxis.range =[changes.points.currentValue[0].x[0], changes.points.currentValue[0].x[changes.points.currentValue[0].x.length - 1]];
-                 Plotly.relayout(graphDiv, this.layout);
-             }
-         }
+                Plotly.restyle(graphDiv, update1);
+                if (changes.points.currentValue.length > 0) {//Changes the range
+                    if (changes.points.currentValue[0].x.length > this.max) {
+                        this.layout.xaxis.range = [changes.points.currentValue[0].x[changes.points.currentValue[0].x.length - (this.max)], changes.points.currentValue[0].x[changes.points.currentValue[0].x.length - 1]];
+                        Plotly.relayout(graphDiv, this.layout)
+                    } else {
+                        this.layout.xaxis.range = [changes.points.currentValue[0].x[0], changes.points.currentValue[0].x[changes.points.currentValue[0].x.length - 1]];
+                        Plotly.relayout(graphDiv, this.layout);
+                    }
+                }
             }
 
-            if (changes.title) {
+            if (changes.title) {//Changes of the Title
                 let update = {
                     title: changes.title.currentValue
                 };
-                Plotly.relayout(graphDiv,update);
+                Plotly.relayout(graphDiv, update);
             }
         }
     }
 
-    private changeYAxisTitle($event){
-        let graphDiv=document.getElementById(this.id);
-        Plotly.relayout(graphDiv, {yaxis:{title:$event}});
+    /**
+     * Updates the YAxis title changed in the modal
+     * @param $event
+     */
+    private changeYAxisTitle($event) {
+        Plotly.relayout(this.getElement(), {yaxis: {title: $event}});
     }
 
-    private changeXAxisTitle($event){
-        let graphDiv=document.getElementById(this.id);
+    /**
+     * Updates the XAxis title changed in the modal
+     * @param $event
+     */
+    private changeXAxisTitle($event) {
         this.layout.xaxis.title = $event;
-        Plotly.relayout(graphDiv, this.layout);
+        Plotly.relayout(this.getElement(), this.layout);
     }
-    private changeTitle($event){
-        let graphDiv=document.getElementById(this.id);
-        this.layout.title = $event;
-        Plotly.relayout(graphDiv, this.layout);
-    }
-
 }
